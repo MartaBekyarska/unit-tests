999c3d70ddfa3e8f93c258f6f69f6fe291e2a636
Marta Bekyarska <martatsoneva@yahoo.com>
2018-09-19 20:44:14 +0100

    Creating tests

--- src/Converter.js
+++ src/Converter.js
@@ -1,4 +1,20 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   //conversion code
-}
+  
+  let farenheit = temperature;
+  let celsius = 20;
+  celsius = (farenheit - 32)/1.8; 
+  return celsius.toFixed(1);
+  }
+  
+
+  function convertCelsiusToFahrenheit(temperature) {
+    //conversion code
+    
+    let fahrenheit = 0;
+    let celsius = temperature;
+    fahrenheit = celsius * 1.8 + 32;
+    return fahrenheit.toFixed(1);
+    }
+  
